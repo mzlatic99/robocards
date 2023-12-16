0d0243e301cf2de36f7c18fe2c41fd400b48b674
Mateo Zlatić <cromateozlatic@gmail.com>
2023-12-16 01:38:46 +0100

    Add styling and title font

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import CardList from './CardList';
 import { robots } from './robots';
 import SearchBox from './SearchBox';
+import './App.css';
 
 class App extends Component {
   constructor() {
@@ -24,7 +25,7 @@ class App extends Component {
     });
     return (
       <div className="tc">
-        <h1>Robo Cards</h1>
+        <h1 className="f2">Robo Cards</h1>
         <SearchBox searchChange={this.onSearchChange} />
         <CardList robots={filteredRobots} />
       </div>
